0007036a439de7e2b49f04744acbc16425cda102
ArkamaHozota <arkamahozota@hotmail.com>
2024-04-22 17:26:16 +0900

    2.08

--- src/main/java/jp/co/sony/ppogah/config/SpringSecurityConfiguration.java
+++ src/main/java/jp/co/sony/ppogah/config/SpringSecurityConfiguration.java
@@ -5,6 +5,7 @@ import javax.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,6 +30,7 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	/**
@@ -85,42 +87,4 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 								.concat(PgCrowd2URLConstants.URL_TO_LOGIN)));
 	}
 
-//	@Bean
-//	protected SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
-//		http.authorizeHttpRequests(
-//				authorize -> authorize.requestMatchers(CrowdProjectURLConstants.URL_STATIC_RESOURCE).permitAll()
-//						.requestMatchers(CrowdProjectURLConstants.URL_EMPLOYEE_TO_PAGES,
-//								CrowdProjectURLConstants.URL_EMPLOYEE_PAGINATION)
-//						.hasAuthority("employee%retrieve")
-//						.requestMatchers(CrowdProjectURLConstants.URL_EMPLOYEE_INFOSAVE,
-//								CrowdProjectURLConstants.URL_EMPLOYEE_INFOUPD,
-//								CrowdProjectURLConstants.URL_EMPLOYEE_TO_ADDITION,
-//								CrowdProjectURLConstants.URL_EMPLOYEE_TO_EDITION)
-//						.hasAuthority("employee%edition").requestMatchers(CrowdProjectURLConstants.URL_EMPLOYEE_DELETE)
-//						.hasAuthority("employee%delete").requestMatchers(CrowdProjectURLConstants.URL_ROLE_TO_PAGES)
-//						.hasAuthority("role%retrieve").anyRequest().authenticated())
-//				.csrf(csrf -> csrf.ignoringRequestMatchers(CrowdProjectURLConstants.URL_STATIC_RESOURCE)
-//						.csrfTokenRepository(new CookieCsrfTokenRepository()))
-//				.exceptionHandling().authenticationEntryPoint((request, response, authenticationException) -> {
-//					final ResponseLoginDto responseResult = new ResponseLoginDto(HttpStatus.UNAUTHORIZED.value(),
-//							authenticationException.getMessage());
-//					CrowdProjectUtils.renderString(response, responseResult);
-//				}).accessDeniedHandler((request, response, accessDeniedException) -> {
-//					final ResponseLoginDto responseResult = new ResponseLoginDto(HttpStatus.FORBIDDEN.value(),
-//							CrowdProjectConstants.MESSAGE_SPRINGSECURITY_REQUIREDAUTH);
-//					CrowdProjectUtils.renderString(response, responseResult);
-//				}).and().formLogin(formLogin -> {
-//					formLogin.loginPage("/pgcrowd/employee/login").loginProcessingUrl("/pgcrowd/employee/do/login")
-//							.defaultSuccessUrl("/pgcrowd/to/mainmenu").permitAll().usernameParameter("loginAcct")
-//							.passwordParameter("userPswd");
-//					try {
-//						formLogin.and().logout(logout -> logout.logoutUrl("/pgcrowd/employee/logout")
-//								.logoutSuccessUrl("/pgcrowd/employee/login"));
-//					} catch (final Exception e) {
-//						throw new CrowdProjectException(CrowdProjectConstants.MESSAGE_STRING_FATAL_ERROR);
-//					}
-//				}).httpBasic(Customizer.withDefaults());
-//		log.info(PgCrowd2Constants.MESSAGE_SPRING_SECURITY);
-//		return http.build();
-//	}
 }
