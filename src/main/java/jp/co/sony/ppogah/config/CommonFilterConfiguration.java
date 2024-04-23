f1b21461fd2f2398c4141706aff2fd8ca1c28143
ArkamaHozota <arkamahozota@hotmail.com>
2024-04-23 15:30:44 +0900

    2.30

--- src/main/java/jp/co/sony/ppogah/config/CommonFilterConfiguration.java
+++ src/main/java/jp/co/sony/ppogah/config/CommonFilterConfiguration.java
@@ -3,7 +3,6 @@ package jp.co.sony.ppogah.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 
 /**
  * CSPFilter配置クラス
@@ -23,7 +22,7 @@ public class CommonFilterConfiguration {
 		// 设置过滤器的名称
 		registrationBean.setName("CSPFilter");
 		// 设置过滤器的顺序
-		registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
+		registrationBean.setOrder(1);
 		// 设置过滤器的 URL 匹配模式
 		registrationBean.addUrlPatterns("/*");
 		// 返回过滤器注册对象
