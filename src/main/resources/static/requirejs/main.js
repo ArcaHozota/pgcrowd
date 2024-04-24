acb5f67fea4c86cd540ae295f6a6d339e70c289f
ArkamaHozota <arkamahozota@hotmail.com>
2024-04-24 14:42:12 +0900

    2.65

--- src/main/resources/static/requirejs/main.js
+++ src/main/resources/static/requirejs/main.js
@@ -1,5 +1,5 @@
 require(['/static/jquery/jquery-3.7.1.min.js'], function() {
-	require(['/static/bootstrap/js/bootstrap.bundle.min.js'], function() {
+	require(['/static/bootstrap/js/bootstrap.min.js'], function() {
 		require(['/static/layer/layer.js', '/static/ztree/jquery.ztree.all.js', '/static/treeview/js/bstreeview.js'], function() {
 			require(['/static/customizes/commons.js'], function() {
 				let jsUrl = document.getElementById("jsContainer").getAttribute('value');
