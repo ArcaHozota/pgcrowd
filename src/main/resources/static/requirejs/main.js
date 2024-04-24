66dbd062db0cf14a1bb3b7d362826e2ef7d503af
ArkamaHozota <arkamahozota@hotmail.com>
2024-04-24 15:34:28 +0900

    2.69

--- src/main/resources/static/requirejs/main.js
+++ src/main/resources/static/requirejs/main.js
@@ -1,9 +1,24 @@
-require(['/static/jquery/jquery-3.7.1.min.js'], function() {
-	require(['/static/bootstrap/js/bootstrap.bundle.min.js'], function(bootstrap) {
-		require(['/static/layer/layer.js', '/static/ztree/jquery.ztree.all.js', '/static/treeview/js/bstreeview.js'], function() {
-			require(['/static/customizes/commons.js'], function() {
-				let jsUrl = document.getElementById("jsContainer").getAttribute('value');
-				require([jsUrl]);
+require.config({
+	paths: {
+		'jquery': '/static/jquery/jquery-3.7.1.min.js',
+		'bootstrap': '/static/bootstrap/js/bootstrap.bundle.min.js'
+	},
+	shim: {
+		'jquery': { exports: 'jquery' },
+		'bootstrap': {
+			deps: ['jquery'], // 依赖 jQuery 或其他库
+			exports: 'bootstrap' // 导出变量名
+		}
+	}
+});
+require(['jquery'], function() {
+	require(['bootstrap'], function() {
+		require(['/static/layer/layer.js'], function() {
+			require(['/static/ztree/jquery.ztree.all.js', '/static/treeview/js/bstreeview.js'], function() {
+				require(['/static/customizes/commons.js'], function() {
+					let jsUrl = document.getElementById("jsContainer").getAttribute('value');
+					require([jsUrl]);
+				});
 			});
 		});
 	});
